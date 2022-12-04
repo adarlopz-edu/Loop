b041a4e3e9fffd6c4b1ac2ade8baa75ba321e8b1
braad <braad@A22100190>
2022-12-03 21:21:19 -0600

    Agregar archivos de proyecto.

---
+++ Loop/Loop.cpp
@@ -0,0 +1,14 @@
+#include "stdio.h"
+#include "dos.h"
+#include "conio.h"
+
+int main() {
+	int F(void);
+	int i;
+
+	for (i = 0;; i++) {
+		printf("%3d Ejecutando ciclo infinito...\n", i);
+	}
+
+}
+
